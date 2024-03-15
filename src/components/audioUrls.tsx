30e0af04ff9d21521ae971e41de8cddf1cc82092
andrewpltnv <andrewpltnv@gmail.com>
2024-03-15 23:30:27 +0200

    audio styles

--- src/components/audioUrls.tsx
+++ src/components/audioUrls.tsx
@@ -1,64 +1,56 @@
-import Timeline from "wavesurfer.js/dist/plugins/timeline.esm.js";
+// import Timeline from "wavesurfer.js/dist/plugins/timeline.esm.js";
 import Hover from "wavesurfer.js/dist/plugins/hover.esm.js";
 import { useWavesurfer } from "@wavesurfer/react";
 import { useLayoutEffect, useMemo, useRef } from "react";
 
 const SoundWave = ({ sound }: { sound?: string }) => {
-  const containerRef = useRef(null);
-  const {
-    wavesurfer,
-    // isPlaying,
-    // currentTime
-  } = useWavesurfer({
-    height: 62,
-    normalize: true,
-    waveColor: "#ffffff",
-    progressColor: "#a51d2d",
-    cursorColor: "#000000",
-    cursorWidth: 4,
-    barWidth: 4,
-    barGap: 2,
-    barAlign: "bottom",
-    barRadius: 19,
-    barHeight: 2.2,
-    fillParent: true,
-    interact: true,
-    hideScrollbar: true,
-    autoScroll: true,
-    autoCenter: true,
-    sampleRate: 24000,
-    container: containerRef,
+	const containerRef = useRef(null);
+	const { wavesurfer } = useWavesurfer({
+		height: 24,
+		normalize: true,
+		waveColor: "#43a4ff94",
+		progressColor: "#a51d2d",
+		cursorColor: "#000000",
+		cursorWidth: 4,
+		barWidth: 4,
+		barGap: 2,
+		barAlign: "bottom",
+		barRadius: 19,
+		barHeight: 2.2,
+		fillParent: true,
+		interact: true,
+		hideScrollbar: true,
+		autoScroll: true,
+		autoCenter: true,
+		sampleRate: 24000,
+		container: containerRef,
 
-    url: sound,
-    plugins: useMemo(
-      () => [
-        Timeline.create(),
-        Hover.create({
-          lineColor: "#ff0000",
-          lineWidth: 4,
-          labelBackground: "#555",
-          labelColor: "#fff",
-          labelSize: "16px",
-        }),
-      ],
-      [],
-    ),
-  });
+		url: sound,
+		plugins: useMemo(
+			() => [
+				// Timeline.create(),
+				Hover.create({
+					lineColor: "--accent",
+					lineWidth: 4,
+					labelBackground: "--destructive",
+					labelColor: "--secondary",
+					labelSize: "16px",
+				}),
+			],
+			[],
+		),
+	});
 
-  useLayoutEffect(() => {
-    // if (settled.current) return;
+	useLayoutEffect(() => {
+		wavesurfer?.on("interaction", () => {
+			wavesurfer.playPause();
+		});
+		wavesurfer?.on("finish", () => {
+			wavesurfer.play();
+		});
+	}, [wavesurfer]);
 
-    wavesurfer?.on("interaction", () => {
-      console.log("plaat");
-
-      wavesurfer.playPause();
-    });
-    wavesurfer?.on("finish", () => {
-      wavesurfer.play();
-    });
-  }, [wavesurfer]);
-
-  return <div className="min-w-0" ref={containerRef} />;
+	return <div className="min-w-0" ref={containerRef} />;
 };
 
 export default SoundWave;
