8a2f463907ed860ca563bc3edcf9a87d2f4c49dc
andrewpltnv <andrewpltnv@gmail.com>
2024-03-15 23:26:23 +0200

    Pokemon Cards changes

--- src/features/Pokemon/PokemonCard.tsx
+++ src/features/Pokemon/PokemonCard.tsx
@@ -1,42 +1,42 @@
 import { Link } from "react-router-dom";
-import { PokemonInfoEntry } from "../../app/types";
+import type { PokemonInfoEntry } from "../../app/types";
 import { Card, Image, Space } from "antd";
 import SoundWave from "../../components/audioUrls";
 import { Type } from "../../components/Type";
 // import { usePermission } from "react-use";
 
 const PokemonCard = (pokemon: PokemonInfoEntry) => {
-  // const permission = usePermission({ name: "speaker" });
-  const {
-    name,
-    types,
-    sprite,
-    // id,
-    cries,
-  } = pokemon;
+	// const permission = usePermission({ name: "speaker" });
+	const {
+		name,
+		types,
+		sprite,
+		// id,
+		cries,
+	} = pokemon;
 
-  return (
-    <Card
-      cover={<Image src={`${sprite}`} className="" />}
-      className="flex h-fit min-w-64 flex-col justify-end "
-      hoverable
-    >
-      <Link to={`/name/${name}`} className="">
-        <Card.Meta
-          style={{ padding: 12 }}
-          title={<h3 className="text-xl">{name.toUpperCase()}</h3>}
-          description={
-            <Space align="baseline" direction="horizontal">
-              {types.map((slot) => (
-                <Type typeSlot={slot} key={slot.type.name} />
-              ))}
-            </Space>
-          }
-        />
-      </Link>
-      <SoundWave sound={cries?.latest} />
-    </Card>
-  );
+	return (
+		<Card
+			cover={<Image src={`${sprite}`} className="" />}
+			className="flex flex-col justify-end min-w-64 h-fit"
+			hoverable
+		>
+			<Link to={`/name/${name}`} className="">
+				<Card.Meta
+					style={{ padding: 12 }}
+					title={<h3 className="text-xl">{name.toUpperCase()}</h3>}
+					description={
+						<Space align="baseline" direction="horizontal">
+							{types.map((slot) => (
+								<Type typeName={slot.type.name} key={slot.type.name} />
+							))}
+						</Space>
+					}
+				/>
+			</Link>
+			<SoundWave sound={cries?.latest} />
+		</Card>
+	);
 };
 
 export default PokemonCard;
--- src/features/Pokemon/PokemonCardSm.tsx
+++ src/features/Pokemon/PokemonCardSm.tsx
@@ -1,40 +1,37 @@
 import { Link } from "react-router-dom";
-import { PokemonInfoEntry } from "../../app/types";
-import { Avatar, Card, Space } from "antd";
+import type { PokemonInfoEntry } from "../../app/types";
 import SoundWave from "../../components/audioUrls";
 import { Type } from "../../components/Type";
+import { twc } from "react-twc";
+
+const Avatar = twc.img`w-20 h-18 float-start`;
 
 const PokemonCardSm = (pokemon: PokemonInfoEntry) => {
-  const {
-    name,
-    types,
-    sprite,
-    // id,
-    cries,
-  } = pokemon;
+	const {
+		name,
+		types,
+		sprite,
+		// id,
+		cries,
+	} = pokemon;
 
-  return (
-    <Card
-      // className="flex h-fit min-w-64 flex-col justify-end "
-      hoverable
-    >
-      <Link to={`/name/${name}`} className="">
-        <Card.Meta
-          style={{ padding: 12 }}
-          title={<h3 className="text-xl">{name.toUpperCase()}</h3>}
-          avatar={<Avatar src={sprite} size="large" shape="square" />}
-          description={
-            <Space align="baseline" direction="horizontal">
-              {types.map((slot) => (
-                <Type typeSlot={slot} key={slot.type.name} />
-              ))}
-            </Space>
-          }
-        />
-      </Link>
-      <SoundWave sound={cries?.latest} />
-    </Card>
-  );
+	return (
+		<div className="flex flex-col bg-blend-color-dodge p-4 rounded-md min-w-52 ring-1 ring-inset ring-ring space-2">
+			<Link
+				to={`/name/${name}`}
+				className="flex justify-between items-center h-full text-center"
+			>
+				<Avatar src={sprite || ""} alt={name} />
+				<h3 className="text-xl">{name.toUpperCase()}</h3>
+			</Link>
+			<SoundWave sound={cries?.latest} />
+			<div className="flex gap-2">
+				{types.map((slot) => (
+					<Type typeName={slot.type.name} key={slot.type.name} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default PokemonCardSm;
