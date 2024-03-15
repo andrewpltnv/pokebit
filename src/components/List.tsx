e0abc4103ad2148d698b27b4d7167f9aa67c1367
andrewpltnv <andrewpltnv@gmail.com>
2024-03-16 01:04:01 +0200

    list cleaner

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,15 +1,15 @@
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import PokemonCardSm from "../features/Pokemon/PokemonCardSm";
 import type { PokemonInfoEntry } from "@/app/types";
+import { twc } from "react-twc";
+
+const ListWrapper = twc.div`place-content-stretch gap-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 p-2 lg:p-8 min-h-screen `;
 
 export function List({ items }: { items: PokemonInfoEntry[] }) {
 	const [animate] = useAutoAnimate();
 
 	return (
-		<div //TODO leverage styles to twc
-			ref={animate}
-			className="place-content-stretch gap-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 p-2 lg:p-8 min-h-screen"
-		>
+		<ListWrapper ref={animate}>
 			{items.map(({ cries, name, id, types, sprite }) => (
 				<PokemonCardSm
 					name={name}
@@ -20,6 +20,6 @@ export function List({ items }: { items: PokemonInfoEntry[] }) {
 					key={name}
 				/>
 			))}
-		</div>
+		</ListWrapper>
 	);
 }
