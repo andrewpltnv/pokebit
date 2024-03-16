d0c64faf6e4898a6c190b583f1ffc5dd71c3b9d1
andrewpltnv <andrewpltnv@gmail.com>
2024-03-16 06:29:56 +0200

    Add font

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "@fontsource-variable/pixelify-sans";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -5,17 +6,17 @@ import { RootLayout } from "./App.tsx";
 import { RouterProvider, createBrowserRouter, json } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
-import PokemonDetailsPage from "./features/Pokemon/PokemonPage.tsx";
 import { ErrorElement } from "./components/ErrorElement.tsx";
 import RootListPage from "./pages/RootList/index.tsx";
 import { PokemonClient } from "pokenode-ts";
 import "@total-typescript/ts-reset";
+import PokemonProfilePage from "./pages/PokemonProfile/index.tsx";
+import ByTypeListPage from "./pages/ByTypeList/index.tsx";
 
-const client = new PokemonClient({ logs: true });
-// TODO move client to Provider
-// TODO folder structure for providers: theme, api
 const root = document.getElementById("root");
 if (!root) throw "There is no root element";
+
+const client = new PokemonClient({ logs: false });
 
 const router = createBrowserRouter([
 	{
@@ -28,18 +29,18 @@ const router = createBrowserRouter([
 				element: <RootListPage />,
 			},
 			{
-				//TODO TypeListingPage
-				// TODO loader for typePage
 				path: "/type/:type",
-				element: <div>Type</div>,
+				element: <ByTypeListPage />,
 				loader: async ({ params }) => {
-					const res = await client.getTypeByName(`${params.type}`);
-					return json(res);
+					const typeInfo = await client.getTypeByName(`${params.type}`);
+
+					return json(typeInfo);
 				},
 			},
 			{
+				//TODO PokemonProfilePage
 				path: "/name/:name",
-				element: <PokemonDetailsPage />,
+				element: <PokemonProfilePage />,
 				loader: async ({ params }) => {
 					const res = await client.getPokemonByName(`${params.name}`);
 					return json(res);
