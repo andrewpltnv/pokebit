764cda4072b4552f00bff600a29b1978d1737282
andrewpltnv <andrewpltnv@gmail.com>
2024-03-16 23:59:42 +0200

    cols

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,7 +3,7 @@ import PokemonCardSm from "../features/Pokemon/PokemonCardSm";
 import { twc } from "react-twc";
 import type { NamedAPIResource } from "pokenode-ts";
 
-const ListWrapper = twc.div`place-content-stretch gap-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 p-2 lg:p-8 min-h-screen `;
+const ListWrapper = twc.div`place-content-stretch gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  p-2 lg:p-8 min-h-screen `;
 
 export function List({ items }: { items: NamedAPIResource[] }) {
 	const [animate] = useAutoAnimate();
